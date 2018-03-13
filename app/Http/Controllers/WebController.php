dc4d7b81312cb951ed92be4b3aeeaec7db4e90d1
Krzysztof Madejski <krzysztof@madejscy.pl>
2018-03-13 09:41:03 -0500

    If resource hasn't been yet scraped serve the original #25

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\ResourceNotIndexedException;
 use App\Repositories\WebRepository;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as LaravelController;
+
+if (!defined('array_any')) {
+    function array_any(array $array, callable $fn)
+    {
+        foreach ($array as $value) {
+            if ($fn($value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
 
 class WebController extends LaravelController
 {
@@ -54,9 +67,24 @@ class WebController extends LaravelController
     public function get($url)
     {
         $url = $this->prepareUrl($url);
-        $object = $this->repo->get($url, [
-            'loadCurrentVersion' => true,
-        ]);
+        try {
+            $object = $this->repo->get($url, [
+                'loadCurrentVersion' => true,
+            ]);
+        } catch (ResourceNotIndexedException $ex) {
+            if (array_any(['.html', '.htm', '/'], function($ends_with) use($url) {
+                return substr($url, -strlen($ends_with)) === $ends_with;
+            })) {
+                // may be page
+                // TODO https://github.com/epforgpl/gov_backup/issues/30
+                // show "Resource hasn't been scraped or resource not known" - see original link
+                throw $ex;
+            } else {
+                // redirect temporary (till this resource will be scraped)
+                // TODO Shouldn't assume http, but use original scheme
+                return redirect()->away('http://' . $url, 302, ['X-GovBackup' =>    'NotScraped-RedirectingToOriginal']);
+            }
+        }
         if ($object) {
             if ($object->hasCurrentVersion()) {
                 return $object->getCurrentVersion()->getBody();
