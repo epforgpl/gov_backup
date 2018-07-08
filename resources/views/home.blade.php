20773c1ce39a2acbc64078dc38d914e44859131d
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2018-07-08 22:13:58 +0200

    Some styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,67 +1,99 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+@show
+
 @section('content')
     <div class="container">
-        <div class="row mt-5">
-            <div class="col-md" style="text-align: center">
-                <h2>Search in archived content</h2>
+        <div id="greeting" class="row">
+            <div class="col-md-8 offset-md-2 text-center">
+                @if (!$textResults)
+                    <a href="/"><img src="{{ asset('img/logo.svg') }}" /></a>
+                    <h2 class="mt-4">Witaj w Archiwum!</h2>
+                    <p class="mt-3">Możesz tutaj przeglądać archiwalne wersje stron rządowych oraz szukać usuniętych z nich treści.</p>
+                @endif
                 <form>
-                    <input type="submit"
-                           style="position: absolute; left: -9999px; width: 1px; height: 1px;"
-                           tabindex="-1"/>
-                    <input name="search" type="text" placeholder="Enter some words" value="{{ $textQuery }}" style="width: 50%;"/>
-                    <br/>
-                    Search deleted phrases <input name="in_deleted" type="checkbox" title="Click to search in deleted phrases"
-                                                  onChange="this.form.submit()" @if($inDeleted)checked @endif>
-            </div>
-        </div>
-        <div class="row mt-5">
-            <div class="col-md" style="text-align: center">
-                <h3>Results</h3>
-                @forelse ($textResults as $r)
-                    <div class="result-item text">
-                        <h4><a href="{{ $r['link'] }}">
-                                {!! $r['highlight']['data.web_objects_versions.title'][0] or $r['data']['web_objects_versions']['title'] !!}
-                            </a>
-                        </h4>
+                    <fieldset>
+                        <div class="form-group form-group-lg">
 
-                        <div>Original link: <a href="{{-- TODO full original link needed --}}http://{{ $r['url'] }}">{!! $r['highlight']['data.web_objects_versions.url'][0] or $r['url'] !!}</a></div>
+                            <div class="input-group mb-3">
+                                <input class="form-control form-control-lg input-main" name="search" type="text" placeholder="Szukaj na stronach rządowych..." value="{{ $textQuery }}" />
 
-                        <div>Available versions: <a href="{{ @EpfHelpers::route_slashed('calendar', [
-                            'url' => $r['url']
-                            // ,'govbackup_query' => $textQuery // TODO filter revisions by query
-                            ])
-                             }}">
-                                {{ $r['versions_count'] }}</a></div>
+                                <div class="input-group-append">
+                                    <input type="submit" class="btn btn-primary button-main" value="Szukaj" />                                </div>
+                            </div>
 
-                        @if ($inDeleted)
-                            <div style="font-weight: bold">"{{ $textQuery }}" was deleted between {{ $r['matching_last_seen']->format('Y-m-d H:i') }}
-                                and {{ $r['not_matching_first_seen']->format('Y-m-d H:i') }}.</div>
-                        @else
-                        <div>This version first seen: {{ $r['first_seen']->format('Y-m-d H:i') }}</div>
-                        <div>This version last seen: {{ $r['last_seen']->format('Y-m-d H:i') }}</div>
-                        @endif
-
-                        {{-- TODO image_id now stores thumbnail (if available), not image_url
-                        @if ($r['data']['web_objects_versions']['image_id'] ?? null)
-                            <img src="{{ $r['data']['web_objects_versions']['image_url'] }}"/>
-                        @endif
-                        --}}
-
-                        <div>{{ $r['data']['web_objects_versions']['description'] }}</div>
-
-                        @if(isset($r['highlight']['text']))
-                        <div class="highlights">
-                            @foreach($r['highlight']['text'] as $h)
-                                <div>{!! $h !!}</div>
-                            @endforeach
                         </div>
-                        @endif
-                    </div>
-                @empty
-                    <div>No results!</div>
-                @endforelse
+                        <div class="form-group mt-3">
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" name="in_deleted" class="custom-control-input" id="onlyDeletedCheckbox" onChange="this.form.submit()" @if($inDeleted)checked @endif>
+                                <label class="custom-control-label" for="onlyDeletedCheckbox">Szukaj tylko w treściach usuniętych</label>
+                            </div>
+                        </div>
+
+                    </fieldset>
+                </form>
             </div>
         </div>
+
+        @if ($textResults)
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <div class="result-items">
+                        @forelse ($textResults as $r)
+                            <div class="result-item text">
+                                <p class="title">
+                                    <a href="{{ $r['link'] }}">
+                                        {!! $r['highlight']['data.web_objects_versions.title'][0] or $r['data']['web_objects_versions']['title'] !!}
+                                    </a>
+                                </p>
+
+                                <p class="link">
+                                    <a href="{{-- TODO full original link needed --}}http://{{ $r['url'] }}">{!! $r['highlight']['data.web_objects_versions.url'][0] or $r['url'] !!}</a>
+                                </p>
+
+                                @if(isset($r['highlight']['text']))
+                                    <div class="highlights">
+                                        @foreach($r['highlight']['text'] as $h)
+                                            <div>{!! $h !!}</div>
+                                        @endforeach
+                                    </div>
+                                @endif
+
+                                <?php /*
+                                <div>Available versions: <a href="{{ @EpfHelpers::route_slashed('calendar', [
+                                    'url' => $r['url']
+                                    // ,'govbackup_query' => $textQuery // TODO filter revisions by query
+                                    ])
+                                     }}">
+                                        {{ $r['versions_count'] }}</a></div>
+
+                                @if ($inDeleted)
+                                    <div style="font-weight: bold">"{{ $textQuery }}" was deleted between {{ $r['matching_last_seen']->format('Y-m-d H:i') }}
+                                        and {{ $r['not_matching_first_seen']->format('Y-m-d H:i') }}.</div>
+                                @else
+                                <div>This version first seen: {{ $r['first_seen']->format('Y-m-d H:i') }}</div>
+                                <div>This version last seen: {{ $r['last_seen']->format('Y-m-d H:i') }}</div>
+                                @endif
+
+                                {{-- TODO image_id now stores thumbnail (if available), not image_url
+                                @if ($r['data']['web_objects_versions']['image_id'] ?? null)
+                                    <img src="{{ $r['data']['web_objects_versions']['image_url'] }}"/>
+                                @endif
+                                --}}
+
+                                <div>{{ $r['data']['web_objects_versions']['description'] }}</div>
+
+
+ */ ?>
+                            </div>
+                        @empty
+                            <div>No results!</div>
+                        @endforelse
+                    </div>
+                </div>
+            </div>
+        @endif
     </div>
 @endsection
--- resources/views/web/view.blade.php
+++ resources/views/web/view.blade.php
@@ -1,64 +1,62 @@
 @extends('layouts.app')
 
-@section('body')
-<div id="page">
+@section('content')
 
-    <div id="header">
-        <div class="navbar navbar-expand-lg">
-            <div class="container-fluid">
-                <!-- TODO correct the title -->
-                <a href="/" class="navbar-brand">GovBackup / Kancelaria Prezesa Rady Ministrów</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
+<?php /*
+<div id="header">
+    <div class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <!-- TODO correct the title -->
+            <a href="/" class="navbar-brand">GovBackup / Kancelaria Prezesa Rady Ministrów</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
 
-                    <!--
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <form action="/search">
-                                <input type="text" name="q" />
-                                <input style="display: none;" type="submit" />
-                            </form>
-                        </li>
-                    </ul>
-                    -->
+                <!--
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <form action="/search">
+                            <input type="text" name="q" />
+                            <input style="display: none;" type="submit" />
+                        </form>
+                    </li>
+                </ul>
+                -->
 
-                    <ul class="nav navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/about" target="_blank">About</a>
-                        </li>
-                    </ul>
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about" target="_blank">About</a>
+                    </li>
+                </ul>
 
-                </div>
             </div>
         </div>
-        <div id="web_object_header">
-            <div class="container-fluid">
-                <div class="web_object_header">
-                    <div class="image">
-                        TODO thumbUrl
-                        {{--
-                        TODO We store only getImageObjectId(), if existing, thumb_url should be computed in controller
-                        @if ($object->getVersion()->getThumbUrl())
-                            <img src="{{ $object->getVersion()->getThumbUrl() }}" />
-                        @endif
-                         --}}
-                    </div>
-                    <div class="content">
-                        <h1>{{ $object->getVersion()->getTitle() }}</h1>
-                        <p class="url">{{ $object->getWebUrl() }}</p>
-                        <p class="revision">{{ $object->getTimestamp()->format('Y-m-d H:i:s') }}</p>
+    </div>
+    <div id="web_object_header">
+        <div class="container-fluid">
+            <div class="web_object_header">
+                <div class="image">
+                    TODO thumbUrl
+                    {{--
+                    TODO We store only getImageObjectId(), if existing, thumb_url should be computed in controller
+                    @if ($object->getVersion()->getThumbUrl())
+                        <img src="{{ $object->getVersion()->getThumbUrl() }}" />
+                    @endif
+                     --}}
+                </div>
+                <div class="content">
+                    <h1>{{ $object->getVersion()->getTitle() }}</h1>
+                    <p class="url">{{ $object->getWebUrl() }}</p>
+                    <p class="revision">{{ $object->getTimestamp()->format('Y-m-d H:i:s') }}</p>
 
-                        @if (config('app.debug'))<p class="object_id">Object ID: {{ $object->getId() }}</p>@endif
-                    </div>
+                    @if (config('app.debug'))<p class="object_id">Object ID: {{ $object->getId() }}</p>@endif
                 </div>
             </div>
         </div>
     </div>
-    <div id="content">
-        <iframe id="iframe" src="{{ $get_url }}"></iframe>
-    </div>
-
 </div>
+*/ ?>
+<iframe id="web_iframe" src="{{ $get_url }}"></iframe>
+
 @endsection
